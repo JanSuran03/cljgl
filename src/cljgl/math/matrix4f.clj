41b061db711167bf3e4260ebbeb2c227761a392f
JanSuran03 <suran.orgpad@gmail.com>
2022-06-13 15:52:15 +0200

    Basic matrix operations.

--- src/cljgl/math/matrix4f.clj
+++ src/cljgl/math/matrix4f.clj
@@ -1,1 +1,13 @@
-(ns cljgl.math.matrix4f)
+(ns cljgl.math.matrix4f
+  (:import (cljgl Matrix4f)))
+
+(defn orthogonal [left right bottom top near far]
+  (Matrix4f/orthogonal left right bottom top near far))
+
+(list :model-matrix :view-matrix :projection-matrix)
+
+(defn model-view-projection-matrix [{:keys [model-matrix view-matrix projection-matrix] :or
+                                     {model-matrix      (Matrix4f/identity)
+                                      view-matrix       (Matrix4f/identity)
+                                      projection-matrix (Matrix4f/identity)}}]
+  (-> projection-matrix (Matrix4f/multiply view-matrix) (Matrix4f/multiply model-matrix)))
---
+++ src/java/cljgl/Matrix4f.java
@@ -0,0 +1,42 @@
+package cljgl;
+
+public class Matrix4f {
+    public static final int SIZE = 4;
+
+    public static float[] blank() {
+        return new float[16];
+    }
+
+    public static float[] identity() {
+        return new float[]{
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1};
+    }
+
+    public static float[] orthogonal(float left, float right, float bottom, float top, float near, float far) {
+        float[] ret = identity();
+        ret[0] = 2.0f / (right - left); // 0 + 0 * 4
+        ret[5] = 2.0f / (top - bottom); // 1 + 1 * 4
+        ret[10] = 2.0f / (near - far); // 2 + 2 * 4
+        ret[12] = (left + right) / (left - right); // 0 + 3 * 4
+        ret[13] = (bottom + top) / (bottom - top); // 1 + 3 * 4
+        ret[14] = (near + far) / (near - far); // 2 + 3 * 4
+        return ret;
+    }
+
+    public static float[] multiply(float[] mat1, float[] mat2) {
+        float[] ret = blank();
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                float sum = 0;
+                for (int i = 0; i < SIZE; i++) {
+                    sum += mat1[row * SIZE + i] * mat2[col * SIZE + i];
+                }
+                ret[SIZE * row + col] = sum;
+            }
+        }
+        return ret;
+    }
+}
