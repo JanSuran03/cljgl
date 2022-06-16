faeb5881e33fdd85df61a9268229fa32ee7626ce
JanSuran03 <suran.orgpad@gmail.com>
2022-06-16 04:06:48 +0200

    Extending renderer done, but buffers still remain untouched!

--- src/cljgl/opengl/renderer.clj
+++ src/cljgl/opengl/renderer.clj
@@ -34,7 +34,7 @@
                    vbo
                    ^Vao vao
                    ebo
-                   ebo-size]
+                   ^:unsynchronized-mutable ebo-size]
   IRenderer
   (render [this]
     (shaders/use-shader-program shader-program)
@@ -81,7 +81,6 @@
                                       (gl/setup-vertex-attribute i components (gl-util/gl-type gl-type)
                                                                  normalize? vertex-buffer-stride byte-offset)
                                       (gl/enable-vertex-attrib-array i)
-
                                       [(inc i) (+ byte-offset (* components (gl-util/sizeof gl-type)))])
                                     [0 0]
                                     attributes-setups))
@@ -89,15 +88,63 @@
     (swap! renderers-by-id assoc renderer-id renderer)
     renderer))
 
-(defn setup-renderer [{:keys [shaders-source-path usage-type vertex-buffer vertex-data
+(defmulti make-renderer (fn [renderer-type data] renderer-type))
+
+(defmethod make-renderer :static-draw
+  [_ {:keys [vertex-data num-attrs order->id indices vertex-buffer-stride attributes-setups
+             renderer-lookup-name shader-program VAO]}]
+  (let [VBO (buffers/static-vbo)
+        EBO (buffers/static-ebo)
+        vertex-data (let [ret (transient [])]
+                      (doseq [vertex vertex-data
+                              i (range num-attrs)
+                              v ((aget order->id i) vertex)]
+                        (conj! ret v))
+                      (persistent! ret))
+        _ (debug/assert-all (buffers/buffer-data VBO vertex-data)
+                            (buffers/buffer-data EBO (int-array indices))
+                            (reduce (fn [[i byte-offset] {:keys [components gl-type normalize?]}]
+                                      (gl/setup-vertex-attribute i components (gl-util/gl-type gl-type)
+                                                                 normalize? vertex-buffer-stride byte-offset)
+
+                                      (gl/enable-vertex-attrib-array i)
+                                      [(inc i) (+ byte-offset (* components (gl-util/sizeof gl-type)))])
+                                    [0 0]
+                                    attributes-setups))
+        renderer (Renderer. renderer-lookup-name shader-program VBO VAO EBO (count indices))]
+    (swap! renderers-by-id assoc renderer-lookup-name renderer)
+    renderer))
+
+(defmethod make-renderer :dynamic-draw
+  [_ {:keys [vertex-buffer-stride attributes-setups renderer-lookup-name shader-program VAO]}]
+  (let [VBO (buffers/dynamic-vbo vertex-buffer-stride)
+        EBO (buffers/dynamic-ebo)
+        _ (debug/assert (reduce (fn [[i byte-offset] {:keys [components gl-type normalize?]}]
+                                  (gl/setup-vertex-attribute i components (gl-util/gl-type gl-type)
+                                                             normalize? vertex-buffer-stride byte-offset)
+
+                                  (gl/enable-vertex-attrib-array i)
+                                  [(inc i) (+ byte-offset (* components (gl-util/sizeof gl-type)))])
+                                [0 0]
+                                attributes-setups))
+        renderer (Renderer. renderer-lookup-name shader-program VBO VAO EBO 0)]
+    (swap! renderers-by-id assoc renderer-lookup-name renderer)
+    renderer))
+
+(defn setup-renderer [{:keys [shaders-source-path usage-type vertex-buffer vertex-data indices
                               shader-program-lookup-name renderer-lookup-name]
                        :or   {shader-program-lookup-name (keyword (gensym "shader-program__"))
                               renderer-lookup-name       (keyword (gensym "renderer__"))}}]
   (let [shader-program (shaders/make-shader-program shader-program-lookup-name shaders-source-path)
         {:keys [attributes-setups]} vertex-buffer
-        vertex-buffer-stride (apply + (map (fn [{:keys [components gl-type]}] (* components (gl-util/sizeof gl-type)))))
-        VAO (buffers/gen-vao)]
-    (case usage-type
-      :static-draw (let [VBO (buffers/static-vbo)
-                         EBO (buffers/static-ebo)]
-                     ))))
+        attributes-setups (map-indexed #(assoc %2 :order %1) attributes-setups)
+        [vertex-buffer-stride order->id] (reduce (fn [[offset order->id] {:keys [components gl-type id order]}]
+                                                   [(+ offset (* components (gl-util/sizeof gl-type)))
+                                                    (assoc order->id order id)])
+                                                 [0 {}]
+                                                 attributes-setups)
+        num-attrs (count attributes-setups)
+        VAO (doto (buffers/gen-vao) buffers/bind)]
+    (make-renderer {:vertex-data          vertex-data :num-attrs num-attrs :order->id order->id :indices indices
+                    :vertex-buffer-stride vertex-buffer-stride :attributes-setups attributes-setups
+                    :renderer-lookup-name renderer-lookup-name :shader-program shader-program :VAO VAO})))
